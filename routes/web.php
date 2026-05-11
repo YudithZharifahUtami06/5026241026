e8df6bf60c19b2e9398c53d1f363de743a5c7ea6
Yudith Zharifah <ifah77.utami@gmail.com>
2026-05-11 11:53:21 +0700

    Pertemuan 9 Kelas A

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiDBController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('menu026');
@@ -57,5 +59,13 @@ Route::get('/blog', function () {
 });
 Route::get('/dosen', [DosenController::class, 'index']);
 Route::get('/biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+//blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
 
 Route::get('/pegawai', [PegawaiDBController::class, 'index']);
