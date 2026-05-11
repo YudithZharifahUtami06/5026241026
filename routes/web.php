aa283d97f977b9cd70c5768634e7ff98265b3a79
Yudith Zharifah <ifah77.utami@gmail.com>
2026-05-11 11:40:59 +0700

    Pertemuan 11 Kelas A

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('menu026');
@@ -56,3 +57,5 @@ Route::get('/blog', function () {
 });
 Route::get('/dosen', [DosenController::class, 'index']);
 Route::get('/biodata', [DosenController::class, 'biodata']);
+
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
